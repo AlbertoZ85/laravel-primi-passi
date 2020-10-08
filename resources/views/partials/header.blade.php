72d1bd765a76211a11ea02b7146e56c8b7698ad1
Alberto Zordan <zorberto@gmail.com>
2020-10-09 00:21:07 +0200

    Prodotti page with some scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,8 @@
 <header>
     <div class="container">
-        <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250,h_145/https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png" alt="la-molisana">
+        <h1>
+            <a href="{{route('home')}}"><img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250,h_145/https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png" alt="la-molisana"></a>
+        </h1>
         <nav>
             <ul>
                 <li><a href="{{route('home')}}">Home</a></li>
